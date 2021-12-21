4dc12806878193d698be5f3acb3b02f8dfe490bf
Aaron Meese <ajmeese7@gmail.com>
2021-12-21 09:05:55 -0500

    Definitely Working
    The styles started getting screwed up after Kara's latest resize request, so I am committing these changes with full confidence that they work before committing the new styles.

--- Music/script.js
+++ Music/script.js
@@ -1,5 +1,4 @@
 import { getId } from "./HelperFunctions.js";
-import { getColor } from "./Color.js";
 import { AudioVisualizer } from "./AudioVisualizer.js";
 const vis = AudioVisualizer;
 const currVis = "waveform";
@@ -31,20 +30,8 @@ function unblockSleep() {
 	}
 }
 
-window.onerror = function (errorMsg, url, lineNumber) {
-	console.log("oof, u got a error\n\n" + url + '[' + lineNumber + ']:\n' + errorMsg);
-}
-
 var audio = new Audio();
 window.audio = audio;
-var audioDuration = 1;
-
-function updateProgress() {
-	progressBar.style.width = audio.currentTime / audioDuration * 100 + "%";
-	progressBar.style.backgroundColor = getColor(audio.currentTime / audioDuration * 255);
-	requestAnimationFrame(updateProgress);
-}
-requestAnimationFrame(updateProgress);
 
 var audioContext;
 var mediaSource;
@@ -53,28 +40,13 @@ var delayNode;
 const setVolume = (newVolume) => audio.volume = newVolume;
 window.setVolume = setVolume;
 
-var songList = getId("songList");
-var progressBar = getId("progress");
 var fileNames = [];
 window.fileNames = fileNames;
-var fileInfo = {};
 var currentSong = -1;
 window.currentSong = currentSong;
 var winsize = [window.innerWidth, window.innerHeight];
 window.size = [window.innerWidth - 8, window.innerHeight - 81];
 var supportedFormats = ['aac', 'aiff', 'wav', 'm4a', 'mp3', 'amr', 'au', 'weba', 'oga', 'wma', 'flac', 'ogg', 'opus', 'webm'];
-
-function listSongs() {
-	var str = "";
-	fileNames.forEach(song => {
-		let songName = song[0];
-		let songNumber = song[1];
-		let songPath = song[2];
-		str += `<div id="song${songNumber}" onclick="selectSong(${songNumber})">${songNumber}: ${songName}</div>`;
-	});
-
-	songList.innerHTML = str;
-}
 
 var currentSong = -1;
 function selectSong(id) {
@@ -83,105 +55,13 @@ function selectSong(id) {
 	audio.currentTime = 0;
 	audio.src = fileNames[id][2];
 	blockSleep();
-	getId("currentlyPlaying").innerHTML = fileNames[id][1] + ": " + fileNames[id][0];
-	document.title = fileNames[id][0] + " - AaronOS Music Player";
+	getId("currentlyPlaying").innerHTML = fileNames[id][0];
 	if (aosToolsConnected) {
 		aosTools.sendRequest({
 			action: "appwindow:set_caption",
-			content: "Music Player - " + fileNames[id][0],
+			content: "Music Player",
 			conversation: "set_caption"
 		});
-	}
-	try {
-		document.getElementsByClassName("selected")[0].classList.remove("selected");
-	} catch (err) {
-		// no song is selected
-	}
-
-	getId("song" + id).classList.add("selected");
-	if (automaticColorCtx) {
-		var autoColorPassed = 0;
-		if (fileInfo[fileNames[id][0]]) {
-			automaticColor = {
-				colorType: fileInfo[fileNames[id][0]].colorType,
-				colorArr: fileInfo[fileNames[id][0]].colors
-			};
-			autoColorPassed = 1;
-		} else if (fileInfo['_default_colors']) {
-			automaticColor = {
-				colorType: fileInfo['_default_colors'].colorType,
-				colorArr: fileInfo['_default_colors'].colors
-			}
-			autoColorPassed = 1;
-		} else {
-			automaticColor = {
-				colorType: 'gradient',
-				colorArr: [
-					[0, 64, 0],
-					[0, 255, 0]
-				]
-			};
-			autoColorPassed = 1;
-		}
-		if (automaticColor.colorArr.length === 0) {
-			if (fileInfo['_default_colors']) {
-				automaticColor = {
-					colorType: fileInfo['_default_colors'].colorType,
-					colorArr: fileInfo['_default_colors'].colors
-				}
-				autoColorPassed = 1;
-			} else {
-				automaticColor = {
-					colorType: 'gradient',
-					colorArr: [
-						[0, 64, 0],
-						[0, 255, 0]
-					]
-				};
-				autoColorPassed = 1;
-			}
-		}
-		if (autoColorPassed) {
-			if (automaticColor.colorArr.length === 1) {
-				automaticColorCtx.fillColor = 'rgba(' + automaticColor.colorArr[0][0] + ',' + automaticColor.colorArr[0][1] + ',' + automaticColor.colorArr[0][2] + (automaticColor.colorArr[0][3] || 1) + ')';
-				automaticColorCtx.fillRect(0, 0, 512, 0);
-			} else {
-				var newGradient = automaticColorCtx.createLinearGradient(0, 0, 512, 0);
-				if (automaticColor.colorType === "gradient") {
-					for (var i = 0; i < automaticColor.colorArr.length; i++) {
-						newGradient.addColorStop(
-							i / (automaticColor.colorArr.length - 1),
-							'rgba(' + automaticColor.colorArr[i][0] + ',' + automaticColor.colorArr[i][1] + ',' + automaticColor.colorArr[i][2] + ',' + (automaticColor.colorArr[i][3] || 1) + ')'
-						);
-					}
-				} else if (automaticColor.colorType === "peak") {
-					if (automaticColor.colorArr.length === 2) {
-						newGradient.addColorStop(
-							0.85,
-							'rgba(' + automaticColor.colorArr[i][0] + ',' + automaticColor.colorArr[i][1] + ',' + automaticColor.colorArr[i][2] + ',' + (automaticColor.colorArr[i][3] || 1) + ')'
-						);
-					} else {
-						for (var i = 0; i < automaticColor.colorArr.length - 1; i++) {
-							newGradient.addColorStop(
-								(i / (automaticColor.colorArr.length - 2)) * 0.85,
-								'rgba(' + automaticColor.colorArr[i][0] + ',' + automaticColor.colorArr[i][1] + ',' + automaticColor.colorArr[i][2] + ',' + (automaticColor.colorArr[i][3] || 1) + ')'
-							);
-						}
-					}
-					newGradient.addColorStop(
-						1,
-						'rgba(' + automaticColor.colorArr[i][0] + ',' + automaticColor.colorArr[i][1] + ',' + automaticColor.colorArr[i][2] + ',' + (automaticColor.colorArr[i][3] || 1) + ')'
-					);
-				}
-				automaticColorCtx.fillStyle = newGradient;
-				automaticColorCtx.fillRect(0, 0, 512, 1);
-			}
-			automaticColor.resultList = [];
-			for (var i = 0; i < 512; i++) {
-				var rgbaResult = automaticColorCtx.getImageData(i, 0, 1, 1).data;
-				automaticColor.resultList.push('rgba(' + rgbaResult[0] + ',' + rgbaResult[1] + ',' + rgbaResult[2] + ',' + rgbaResult[3] + ')');
-			}
-		}
 	}
 }
 window.selectSong = selectSong;
@@ -190,36 +70,24 @@ window.selectSong = selectSong;
 window.toggleAudioState = () => audio.paused ? play() : pause();
 function play() {
 	if (currentSong === -1) {
-		selectSong(0);
+		shuffle();
 	} else {
 		audio.play();
 		blockSleep();
 	}
 
-	getId("playbutton").innerHTML = "<b>&nbsp;||&nbsp;</b>";
+	getId("playpauseicon").src = "pause.svg";
 }
 function pause() {
 	audio.pause();
 	unblockSleep();
-	getId("playbutton").innerHTML = "&#9658;";
+	getId("playpauseicon").src = "play.svg";
 }
 
 function firstPlay() {
-	audioDuration = audio.duration;
 	play();
 }
 audio.addEventListener("canplaythrough", firstPlay);
-
-function setProgress(e) {
-	if (currentSong == -1) return;
-
-	let timeToSet = e.pageX - 5;
-	if (timeToSet < 5) timeToSet = 0;
-	timeToSet /= size[0];
-	timeToSet *= audio.duration;
-	audio.currentTime = timeToSet;
-}
-window.setProgress = setProgress;
 
 function back() {
 	if (audio.currentTime >= 3) return audio.currentTime = 0;
@@ -237,7 +105,7 @@ function next() {
 window.next = next;
 
 function songEnd() {
-	if (!willWindowClose()) next();
+	next();
 }
 
 audio.addEventListener("ended", songEnd);
@@ -255,10 +123,8 @@ function shuffle() {
 	audio.currentTime = 0;
 	currentSong = 0;
 	shuffleArray(fileNames);
-	listSongs();
 	selectSong(0);
 }
-window.shuffle = shuffle;
 
 var perfLast = performance.now();
 var perfCurrent = perfLast;
@@ -271,21 +137,16 @@ function globalFrame() {
 	if (winsize[0] !== window.innerWidth || winsize[1] !== window.innerHeight) {
 		winsize = [window.innerWidth, window.innerHeight];
 		window.size = [window.innerWidth - 8, window.innerHeight - 81];
-		// TODO!
-		getId("visCanvas").width = window.size[0]; // * 0.7;
-		getId("visCanvas").height = window.size[1]; // * 0.7;
-		if (currVis !== "none" && vis[currVis].sizechange) {
-			vis[currVis].sizechange();
-		}
+		getId("visCanvas").width = window.size[0];
+		getId("visCanvas").height = window.size[1];
 	}
 
-	if (currVis == "none") return;
 	window.analyser.getByteFrequencyData(window.visData);
 
 	// If the audio's volume is lowered, the visualizer can't hear it, so
 	// this attempts to artificially bring the volume back up to full
 	if (audio.volume < 0.9) {
-		var gainFactor = 0.9 - audio.volume + 1;
+		let gainFactor = 0.9 - audio.volume + 1;
 		for (let i = 0; i < window.visData.length; i++) {
 			window.visData[i] = Math.floor(window.visData[i] * gainFactor);
 		}
@@ -300,15 +161,6 @@ function globalFrame() {
 	vis[currVis].frame();
 }
 
-var automaticColorCtx = null;
-var automaticColor = {
-	colorType: 'peak',
-	colorArr: ['#005500', '#00FF00', '#FF0000'],
-	resultList: new Array(512)
-}
-
-progressBar.style.outline = "2px solid " + getColor(255);
-
 function loadAudio() {
 	// https://stackoverflow.com/a/55270278/6456163
 	let files = [];
@@ -320,10 +172,11 @@ function loadAudio() {
 		let HTML = parser.parseFromString(xhr.response, 'text/html');
 		let elements = HTML.getElementsByTagName("a");
 		for (let x of elements) {
-			let fileName = x.href;
-			let fileNameParts = fileName.split("Music");
-			if (supportedFormats.includes(fileName.split(".")[2])) {
-				files.push(fileNameParts[0] + "Music/audio" + fileNameParts[1]);
+			let filePath = x.href;
+			let filePathParts = filePath.split("Music");
+			if (supportedFormats.includes(filePath.split(".")[2])) {
+				// Push the correct file path to the files array
+				files.push(filePathParts[0] + "Music/audio" + filePathParts[1]);
 			}
 		};
 	}
@@ -339,10 +192,9 @@ function loadAudioFiles() {
 
 	for (let i = 0; i < filePaths.length; i++) {
 		let fileName = filePaths[i].split('audio/')[1].split(".")[0];
-		fileNames.push([fileName, i, filePaths[i]]);
+		fileNames.push([decodeURI(fileName), i, filePaths[i]]);
 	}
 
-	listSongs();
 	audioContext = new AudioContext({
 		latencyHint: 'playback',
 		//sampleRate: 8000,
@@ -370,18 +222,3 @@ function loadAudioFiles() {
 	requestAnimationFrame(globalFrame);
 }
 loadAudioFiles();
-
-var selfCloseEnabled = 0;
-var selfCloseSongs = 10;
-
-function willWindowClose() {
-	if (!selfCloseEnabled) return 0;
-
-	selfCloseSongs--;
-	if (selfCloseSongs > 0) return 0;
-	getId("currentlyPlaying").innerHTML = "Window will close in 5 seconds."
-	setTimeout(() => {
-		remote.getCurrentWindow().close();
-	}, 5000);
-	return 1;
-}
--- apps/music_player.js
+++ apps/music_player.js
@@ -20,16 +20,13 @@ apps.musicPlayer = new Application({
 				<iframe
 					data-parent-app="musicPlayer"
 					id="MPlframe"
-					onload="apps.musicPlayer.vars.updateStyle()"
 					style="border:none; display:block; width:100%; height:100%; overflow:hidden;"
 					src="./Music/index.html"
 				></iframe>`
 			);
-			requestAnimationFrame(this.vars.colorWindows);
 			getId("icn_musicPlayer").style.display = "inline-block";
 			requestAnimationFrame(() => {
 				this.appWindow.appIcon = 1;
-				this.vars.colorWindows();
 			});
 		}
 		this.appWindow.setCaption('Music Player');
@@ -38,27 +35,6 @@ apps.musicPlayer = new Application({
 		blockScreensaver("apps.musicVis");
 		if (this.appWindow.appIcon) {
 			this.appWindow.openWindow();
-		}
-	},
-	vars: {
-		//appInfo: 'This is the official AaronOS Music Player. Select a folder of songs to loop through.',
-		updateStyle: function() {},
-		colorModified: 0,
-		colorWindows: function() {
-			if (apps.musicPlayer.appWindow.appIcon) {
-				let MPlTitle = getId("MPlframe").contentDocument.title;
-				if (MPlTitle.indexOf("WindowRecolor:") === 0) {
-					apps.settings.vars.setWinColor(1, MPlTitle.split(":")[1]);
-					if (!this.colorModified) this.colorModified = 1;
-				} else if (this.colorModified) {
-					apps.settings.vars.setWinColor(1, ufload("aos_system/windows/border_color") || 'rgba(150, 150, 200, 0.5)');
-					this.colorModified = 0;
-				}
-				requestAnimationFrame(apps.musicPlayer.vars.colorWindows);
-			} else if (this.colorModified) {
-				apps.settings.vars.setWinColor(1, ufload("aos_system/windows/border_color") || 'rgba(150, 150, 200, 0.5)');
-				this.colorModified = 0;
-			}
 		}
 	}
 });
