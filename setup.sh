34b3ba126dcbe1113bd4122269c8150036271255
Aaron Meese <ajmeese7@gmail.com>
2022-01-16 10:37:26 -0500

    Bash safety features
    Added syntax in case `cd` fails somehow, so the script will exit with an error code instead of failing silently.  Also renamed files for case-sensitive Linux.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install PHP
-if [ command -v php -ne "/usr/bin/php" ];
+if command -v php -ne "/usr/bin/php";
 then
 	sudo apt install apt-transport-https lsb-release ca-certificates wget -y
 	sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg 
@@ -14,11 +14,11 @@ else
 fi
 
 # Install nvm for Node installation versioning
-if [ command -v nvm -ne "nvm" ];
+if command -v nvm -ne "nvm";
 then
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 fi
-nvm install 12.22.9
+nvm install 12
 sudo npm install -g yarn
 
 # Install python2 for compatibility
@@ -26,6 +26,6 @@ sudo apt-get update
 sudo apt-get install -y python2 build-essential gcc g++ make libkrb5-dev libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 
 # set up old site app
-cd old-site
+cd old-site || exit 1
 yarn install
 yarn run build
