97917063f8762825ea93f588f7c8b841f3ee9a27
Aaron Meese <ajmeese7@gmail.com>
2022-05-24 19:41:13 -0400

    Improved browser functionality
    We now use the Google API to get favicons for the search results URL much like DuckDuckGo does in their search results.

--- googleSearch.php
+++ googleSearch.php
@@ -12,18 +12,23 @@ $client = Client::createChromeClient();
 
 # Crawl the Google page of interest and wait for the JavaScript to load
 $query = urlencode($_GET["q"]);
-$client->get("http://www.google.com/search?hl=en&tbo=d&site=&source=hp&q=".$query."&oq=".$query);
+$client->get("http://www.google.com/search?hl=en&tbo=d&site=&source=hp&q=".$query);
 $crawler = $client->waitFor("#search");
 $html = $crawler->html();
 
 # Parse the HTML and get the results elements
 $document = new Document($html);
+$difficult_elements = $document->find('h3[role="heading"]');
+foreach ($difficult_elements as $element) {
+	# Removes several Google elements that are difficult to parse later on
+	$element->remove();
+}
 $search = $document->find("[data-async-context*=query]")[0];
 $results = $search->children();
 
 /** Contains Google selectors that we aren't interested in returning. */
 $ignore = [
-	// "People also ask" results
+	// Removes "People also ask" results
 	"data-initq",
 	// These shouldn't exist in this section, but removes any ads if they do
 	"data-text-ad",
@@ -31,12 +36,13 @@ $ignore = [
 	"Local Results",
 	// Removes YouTube and Twitter cards
 	"g-scrolling-carousel",
+	// Removes news that nobody should be interested in
+	"Top stories",
 ];
 
 # Filters out all the $ignore selectors from the results.
+# https://stackoverflow.com/a/33987987/6456163
 $relevantResults = array_filter($results, function($result) use ($ignore) {
-	# https://stackoverflow.com/a/33987987/6456163
-
 	# Remove all script tags
 	if (!empty($result->first("script"))) {
 		foreach($result->findInDocument("script") as $script) {
@@ -65,6 +71,14 @@ $relevantResults = array_map(function($result) {
 		return $result->html();
 	}
 }, $relevantResults);
+
+# Filter out all the results that don't have h3 tags.
+$relevantResults = array_filter($relevantResults, function($result) {
+	$document = new Document($result);
+	$h3_elements = $document->find("h3");
+
+	return isset($h3_elements[0]);
+});
 
 # Get only the data we care about from the HTML.
 $resultData = array_map(function($result) {
